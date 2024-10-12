9949d79b0b9a28b3b3aee2c2868478a28ba6be82
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-10-12 11:35:38 +0530

    backend done

--- notes_app/noteapp/urls.py
+++ notes_app/noteapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from noteapp.views import notes_view
+from noteapp.views import notes_view, note_detail_view
 
 urlpatterns = [
     path('notes/', notes_view, name= "notes"),
+    path('notes/<slug:slug>/', note_detail_view, name= "note_detail"),
 ]

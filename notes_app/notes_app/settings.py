f4fe9bd60a02d53a3abc4b783cd51e4fb4008f2f
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-10-18 14:15:19 +0530

    installed & configured django cors headers

--- notes_app/notes_app/settings.py
+++ notes_app/notes_app/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'noteapp',
     'rest_framework',
+    'corsheaders',  # Install django cors headers and then do few configuration like this and in middleware and add domains which can access the API
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -71,6 +73,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'notes_app.wsgi.application'
 
+CORS_ALLOWED_ORIGINS = [
+"http://localhost:5173",
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases

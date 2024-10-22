efae15356a8c5438b4d40137344bc3e0ffb4fb0a
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-10-22 21:59:28 +0530

    search view added

--- notes_app/noteapp/urls.py
+++ notes_app/noteapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from noteapp.views import notes_view, note_detail_view
+from noteapp.views import notes_view, note_detail_view, search_notes_view
 
 urlpatterns = [
     path('notes/', notes_view, name= "notes"),
     path('notes/<slug:slug>/', note_detail_view, name= "note_detail"),
+    path('notes-search/', search_notes_view, name='notes-search')
 ]
--- notes_app/noteapp/views.py
+++ notes_app/noteapp/views.py
@@ -4,8 +4,16 @@ from noteapp.models import Note
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from django.db.models import Q
 
 # Create your views here.
+@api_view(['GET'])
+def search_notes_view(request):
+    query = request.query_params.get('search')
+    notes = Note.objects.filter(Q(title__icontains = query) | Q(body__icontains = query) | Q(category__icontains = query))
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
 @api_view(['GET', 'POST'])
 def notes_view(request):
     if request.method == 'GET':

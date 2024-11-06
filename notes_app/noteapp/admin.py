60f3286c9361eb21518b4a746f204e4551e8f2ec
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-11-06 20:16:40 +0530

    Added User field to NoteAdmin list_display

--- notes_app/noteapp/admin.py
+++ notes_app/noteapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Note
+from django.contrib.auth.models import User
 
 # Register your models here.
 
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'created_at', 'updated_at']
+    list_display = ['title', 'category', 'created_at', 'updated_at', 'user']
 admin.site.register(Note, NoteAdmin)

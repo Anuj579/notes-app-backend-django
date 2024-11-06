16aa62deff5fcdd9a944d0bafdefca17a4ce8f8a
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-11-06 20:53:09 +0530

    Updated NoteAdmin in admin.py: reordered list_display fields

--- notes_app/noteapp/admin.py
+++ notes_app/noteapp/admin.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 # Register your models here.
 
+
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'created_at', 'updated_at', 'user']
+    list_display = ["title", "user", "category", "created_at", "updated_at"]
+
+
 admin.site.register(Note, NoteAdmin)

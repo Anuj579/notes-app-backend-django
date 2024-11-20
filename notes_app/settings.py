55d2949229be04e7b597d6ce9fe6a577f6db9edc
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-11-21 00:04:34 +0530

    Update FRONTEND_URL to live url

--- notes_app/settings.py
+++ notes_app/settings.py
@@ -156,7 +156,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-FRONTEND_URL = "http://localhost:5173"
+FRONTEND_URL = "https://noteworthyonline.netlify.app"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
